1774b7bdf7f3cc485c718d62a527fc9e2c4935fb
APIVerve Team <hello@apiverve.com>
2024-05-13 22:35:31 -0500

    Initial Commit

---
+++ QueryOptions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class QueryOptions {
+public float lat1 { get; set; }
+public float lon1 { get; set; }
+public float lat2 { get; set; }
+public float lon2 { get; set; }
+}
+
+}
